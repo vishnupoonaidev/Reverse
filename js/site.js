b94a7cd41c12044770efd72b0cb809a33b0be4eb
Vishnu Poonai <52576540+vishnu277@users.noreply.github.com>
2023-01-18 17:25:31 -0500

    project complete except logos and links

--- js/site.js
+++ js/site.js
@@ -1,4 +1,38 @@
-//Call HelloWorld
-function HelloWorld(){
-    alert("hello World");
+//get the string from the page 
+//controller function
+function getValue(){
+
+    document.getElementById("alert").classList.add("invisible")
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+    
+    displayString(revString);
+
+}
+
+
+//reverse the string
+//logic function
+function reverseString(userString){
+ let revString = [];
+
+ //reverse a string using a for loop
+ for (let index = userString.length -1; index >=  0; index--) {
+    revString += userString[index];
+ }
+     return revString;
+}
+
+
+//display the reverses string to the user
+//view function
+function displayString(revString){
+
+    //write message to the page
+    document.getElementById("msg").innerHTML = `your reversed string is: ${revString}`;
+
+    //show the alert box
+   document.getElementById("alert").classList.remove("invisible");
 }
